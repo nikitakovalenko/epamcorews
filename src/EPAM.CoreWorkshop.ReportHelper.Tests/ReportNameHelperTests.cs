4bb4197bd4716688000719a14fdcdef2b8c9b39a
Nikita Kovalenko <Nikita_Kovalenko@epam.com>
2018-10-13 13:03:27 +0400

    Added delay

--- src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
+++ src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -6,6 +7,12 @@ namespace EPAM.CoreWorkshop.ReportHelper.Tests
 	[TestClass]
 	public class ReportNameHelperTests
 	{
+		[AssemblyInitialize]
+		public static void Init(TestContext context)
+		{
+			Thread.Sleep(30 * 1000);
+		}
+
 		[TestMethod]
 		[DataRow("Hello: my world", '_', "Hello_ my world")]
 		[DataRow("Hello my %world", '_', "Hello my %world")]
